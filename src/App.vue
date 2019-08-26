c590cde74f6358cf5c2fe7e717eee9d43ccecb76
zhangwenlong <2543068611@qq.com>
2019-08-26 19:07:26 +0800

    框架

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="app">
+    <div id="nav">
+      <Home />
+      <!-- <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>-->
+    </div>
+    <!-- <router-view /> -->
+  </div>
+</template>
+<script lang="ts">
+import Vue from "vue"
+import Home from '@/views/Home';
+export default Vue.extend({
+  components: {
+    Home
+  }
+});
+</script>
+<style lang="scss">
+* {
+  padding: 0;
+  margin: 0;
+  list-style: none;
+  text-decoration: none;
+  box-sizing: border-box;
+}
+html, 
+body,
+#app {
+  width: 100%;
+  height: 100%;
+}
+#nav {
+  width: 100%;
+  height: 100%;
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,103 @@
+<template>
+  <div class="home">
+    <!-- <div class="top">
+      <p>重点学生日周考成绩录入管理系统</p>
+    </div> -->
+    <!-- <div class="cont"></div> -->
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+//import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+
+export default Vue.extend({
+  name: 'home',
+  components: {
+   
+  },
+  created(){
+    // 变量限制
+    let a:any = '100';
+
+    // a.a();
+
+    let list: object[] = [{}, {}, {}];
+    let list2: Array<number> = [1, 2, 3];
+  
+    // 函数限制
+    function func(a:any, b:any, c?:any): number{
+      return 100;
+    }
+
+    func(1, 2);
+
+    // 自定义数据结构
+    interface ReduceItem{
+      class: string,
+      weight: number,
+      romm?: number 
+    }
+
+    let classes: Array<ReduceItem> = [{
+      class: '1612B',
+      weight: 500
+    }, {
+      class: '1612A',
+      weight: 400
+    }, {
+      class: '1612C',
+      weight: 499
+    }]
+
+    classes.sort((a:ReduceItem, b:ReduceItem)=>a.weight-b.weight);
+    console.log('classes...', classes);
+
+     // 泛型
+    function minValue<T>(arrs: T[]): T{
+      if (typeof arrs[0] == 'string'){
+        arrs.sort((a:any, b:any)=>a-b);
+        return arrs[0];
+      }else if(typeof arrs[0] == 'number'){
+        arrs.sort((a:any, b:any)=>a-b);
+        return arrs[0];
+      }else if(typeof arrs[0] == 'object'){
+        arrs.sort((a:any, b:any)=>a.weight-b.weight);
+        return arrs[0];
+      }else{
+        return arrs[0];
+      }
+    }
+    
+    console.log(minValue([1,2,3,4,5,6]));
+
+    console.log(minValue(['a','b','c','4','5','6']));
+
+    console.log(minValue(classes))
+    
+  }
+});
+</script>
+<style lang="scss" scoped>
+.home {
+  width: 100%;
+  height: 100%;
+}
+.top {
+  width: 100%;
+  height: 100px;
+  background: #3F51B5;
+  p {
+    text-align: center;
+    line-height: 100px;
+    color: white;
+    font-size: 20px
+  }
+}
+.cont {
+  width: 499px;
+  height: 471px;
+  border: 1px solid #ccc;
+  margin: 144px auto; 
+}
+</style>
